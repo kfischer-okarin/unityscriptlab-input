d876345afaaf67294baa6916ca8a76b380495348
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-03-28 15:29:56 +0900

    Add InputAction

---
+++ UnityScriptLab/Input/InputAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace UnityScriptLab {
+    namespace Input {
+        /// <summary>
+        /// Input Action.
+        /// </summary>
+        public class InputAction {
+
+            static Dictionary<string, InputAction> actions = new Dictionary<string, InputAction> ();
+
+            string name;
+
+            public static InputAction Get (string name) {
+                if (!actions.ContainsKey (name)) {
+                    actions[name] = new InputAction (name);
+                }
+                return actions[name];
+            }
+
+            private InputAction (string name) {
+                this.name = name;
+            }
+
+        }
+    }
+}

26442a06b5680e8182d67b8083689e3058f2249d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-03-29 17:10:57 +0900

    Add ButtonPressed

--- UnityScriptLab/Input/Button.cs
+++ UnityScriptLab/Input/Button.cs
@@ -3,16 +3,29 @@
 namespace UnityScriptLab {
     namespace Input {
         public class Button : InputControl {
+            public static Button ButtonPressed(string buttonName) {
+                return new Button(buttonName, State.Pressed);
+            }
+
+            enum State {
+                Pressed,
+            }
             string buttonName;
-            public Button(string buttonName) {
+            State buttonState;
+            private Button(string buttonName, State buttonState) {
                 this.buttonName = buttonName;
+                this.buttonState = buttonState;
             }
 
             public event Action Triggered;
 
             public void Update() {
-                if (UnityEngine.Input.GetButtonDown(buttonName)) {
-                    Triggered?.Invoke();
+                switch (buttonState) {
+                    case State.Pressed:
+                        if (UnityEngine.Input.GetButtonDown(buttonName)) {
+                            Triggered?.Invoke();
+                        }
+                        break;
                 }
             }
         }

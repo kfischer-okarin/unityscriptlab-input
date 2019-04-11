8db070adbf1ac6cc0933b791c64281c881bad856
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-04-11 14:51:08 +0900

    Rewrite

--- UnityScriptLab/Input/Event/Key.cs
+++ UnityScriptLab/Input/Event/Key.cs
@@ -7,15 +7,13 @@ namespace UnityScriptLab {
         namespace Event {
 
             public static class Key {
-                public static InputEvent Pressed(KeyCode key) {
-                    return new InputEvent($"KeyPressed-{key}", input => input.GetKeyDown(key));
-                }
-                public static InputEvent Released(KeyCode key) {
-                    return new InputEvent($"KeyReleased-{key}", input => input.GetKeyUp(key));
-                }
-                public static InputEvent Held(KeyCode key) {
-                    return new InputEvent($"KeyHeld-{key}", input => input.GetKey(key), input => input.GetKeyUp(key));
-                }
+                public static InputEvent Pressed(KeyCode key) => new InputEvent($"KeyPressed-{key}",
+                    input => input.GetKeyDown(key));
+                public static InputEvent Released(KeyCode key) => new InputEvent($"KeyReleased-{key}",
+                    input => input.GetKeyUp(key));
+                public static InputEvent Held(KeyCode key) => new InputEvent($"KeyHeld-{key}",
+                    input => input.GetKey(key),
+                    input => input.GetKeyUp(key));
             }
         }
     }

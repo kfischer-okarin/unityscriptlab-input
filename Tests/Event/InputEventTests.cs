57c546e1fda05671fd76c27c06a8942f1d978013
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-04-12 14:32:23 +0900

    Add InputEvent tests

---
+++ Tests/Event/InputEventTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using UnityScriptLab.Input;
+using UnityScriptLab.Input.Event;
+
+namespace Tests {
+    namespace Event {
+        public class InputEventTests {
+            NUnit.Framework.Constraints.Constraint ContainsExactly(params InputEvent[] evs) {
+                return Is.EquivalentTo(evs);
+            }
+
+            [Test]
+            public void BoundEventListTest() {
+                InputEvent.ResetBindings();
+
+                InputEvent ev1 = Key.Pressed(KeyCode.Space);
+                InputEvent ev2 = Key.Pressed(KeyCode.Return);
+                Action action = () => { };
+                Action secondAction = () => { };
+
+                ev1.Triggered += action;
+                ev1.Stopped += secondAction;
+                ev2.Stopped += action;
+                Assert.That(InputEvent.BoundEvents, ContainsExactly(ev1, ev2));
+
+                ev1.Triggered -= action;
+                Assert.That(InputEvent.BoundEvents, ContainsExactly(ev1, ev2));
+
+                ev1.Stopped -= secondAction;
+                Assert.That(InputEvent.BoundEvents, ContainsExactly(ev2));
+
+                InputEvent.ResetBindings();
+                Assert.That(InputEvent.BoundEvents, Is.Empty);
+            }
+        }
+    }
+}
